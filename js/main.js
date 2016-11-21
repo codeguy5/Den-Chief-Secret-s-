6617513f02e76875f052c9d6f88fdf842982e120
codeguy5 <stephenplehr@gmail.com>
2016-11-21 13:42:05 -0500

    sorry

--- js/main.js
+++ js/main.js
@@ -7,3 +7,10 @@ $(document).ready(function() {
     }, 3000);
 
 });
+script>
+// When the user clicks on <div>, open the popup
+function myFunction() {
+    var popup = document.getElementById('myPopup');
+    popup.classList.toggle('show');
+}
+</script>
