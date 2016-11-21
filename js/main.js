2a2612c621eca39d92e3118c0803ff27bfe08e3f
codeguy5 <stephenplehr@gmail.com>
2016-11-21 16:09:35 -0500

    bug fix

--- js/main.js
+++ js/main.js
@@ -1,16 +1,4 @@
 $('body').toggleClass('loaded');
 $(document).ready(function() {
 
-    setTimeout(function(){
-        $('body').addClass('loaded');
-        $('h1').css('color','#222222');
-    }, 3000);
-
-});
-script>
-// When the user clicks on <div>, open the popup
-function myFunction() {
-    var popup = document.getElementById('myPopup');
-    popup.classList.toggle('show');
-}
-</script>
+    
