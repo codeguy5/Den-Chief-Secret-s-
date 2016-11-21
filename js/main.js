852ba856d460d058ceaf098f152b31ef61988eca
codeguy5 <stephenplehr@gmail.com>
2016-11-20 19:11:20 -0500

    initial

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$('body').toggleClass('loaded');
+$(document).ready(function() {
+
+    setTimeout(function(){
+        $('body').addClass('loaded');
+        $('h1').css('color','#222222');
+    }, 3000);
+
+});
