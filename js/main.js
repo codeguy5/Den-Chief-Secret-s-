d2d38e8b1245b60f592cf07075f7da0533a20151
codeguy5 <stephenplehr@gmail.com>
2016-11-25 14:03:38 -0500

    winter update

--- js/main.js
+++ js/main.js
@@ -2,4 +2,21 @@ $('body').toggleClass('loaded');
 $(document).ready(function() {
 })
  window.alert("NEW DOMAIN YES!!!"); 
- 
+ snowfall({collection : 'element'});
+ $(document).snowfall({collection : '.elements'});
+$(document).snowfall({collection : '#element'});
+$(document).snowfall();
+$('#elementid').snowfall({flakeCount : 100, maxSpeed : 10});
+$('.class').snowfall({flakeCount : 100, maxSpeed : 10});
+options = {
+    flakeCount : 35,        // number
+    flakeColor : '#ffffff', // string
+    flakeIndex: 999999,     // number
+    minSize : 1,            // number
+    maxSize : 3,            // number
+    minSpeed : 2,           // number
+    maxSpeed : 3,           // number
+    round : false,          // bool
+    shadow : false,         // bool
+        collection : 'element'          // string
+};
