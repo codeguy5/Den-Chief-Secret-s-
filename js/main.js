1176ca80e20b8ff5eaec8688d13589a11743e6e7
codeguy5 <stephenplehr@gmail.com>
2016-11-25 13:43:46 -0500

    bug fix 3

--- js/main.js
+++ js/main.js
@@ -2,8 +2,4 @@ $('body').toggleClass('loaded');
 $(document).ready(function() {
 }
  window.alert("NEW DOMAIN YES!!!"); 
- window.hasOwnProperty('alert'); // false
-window.alert = null;
-window.hasOwnProperty('alert'); // true
-delete window.alert;
-window.hasOwnProperty('alert'); // false
+ 
